92969dde13bdbb02385cc855cfbb3540486c1107
John <John@DESKTOP-SRO1BKN>
2021-09-18 01:48:02 -0700

    Experimenting with state

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
-import { StyleSheet } from "react-native";
 
 // Components
 import HomeScreen from "./components/HomeScreen";
@@ -39,40 +38,3 @@ function App() {
 }
 
 export default App;
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-
-    viewOne: {
-        flex: 1,
-        width: "100%",
-        backgroundColor: "darkblue",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    viewTwo: {
-        flex: 2,
-        width: "100%",
-        backgroundColor: "powderblue",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-
-    viewThree: {
-        flex: 3,
-        width: "100%",
-        backgroundColor: "steelblue",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-
-    catPhoto: { resizeMode: "stretch", width: 250, height: 300 },
-
-    text: {
-        color: "orange",
-    },
-});
--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,36 +1,40 @@
 import React, { useState, useLayoutEffect } from "react";
 import { Text, View, Button } from "react-native";
+import styles from "../styles";
 
 const HomeScreen = ({ navigation }) => {
     const [count, setCount] = useState(0);
 
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            headerRight: () => (
+    return (
+        <>
+            <Text>Count: {count}</Text>
+            <View
+                style={{
+                    flex: 1,
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flexDirection: "row",
+                }}
+            >
                 <Button
+                    style={styles.leftButton}
                     onPress={() => setCount((count) => count + 1)}
                     title="Update Count"
                 />
-            ),
-        });
-    }, [navigation, setCount]);
-
-    return (
-        <View
-            style={{
-                flex: 1,
-                alignItems: "center",
-                justifyContent: "center",
-            }}
-        >
-            <Text>Count: {count}</Text>
-            <Button
-                title="Go to Details"
-                onPress={() => {
-                    navigation.navigate("Details");
-                }}
-            />
-        </View>
+                <Button
+                    style
+                    title="Go to Details"
+                    onPress={() => {
+                        navigation.navigate("Details");
+                    }}
+                />
+                <Button
+                    style="right-button"
+                    onPress={() => setCount((count) => count + 1)}
+                    title="Update Count"
+                />
+            </View>
+        </>
     );
 };
 
